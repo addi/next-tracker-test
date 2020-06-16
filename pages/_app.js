825af9db314a8f6074de147039137648c22bf7cc
Árni Jónsson <addininja@gmail.com>
2020-06-17 00:12:36 +0200

    Added id syncing between tabs.

--- pages/_app.js
+++ pages/_app.js
@@ -2,28 +2,52 @@ import React, { useEffect } from "react";
 
 import Router from "next/router";
 
-const trackUrl = async (path) => {
-  let response = await fetch("/api/track", {
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify({ path: path }),
-  });
-};
-
-const handleRouteChange = (url) => {
-  console.log("App is changed to: ", url);
-  trackUrl(Router.pathname);
-};
-
-Router.events.on("routeChangeComplete", handleRouteChange);
+var sID = undefined;
 
 export default function App({ Component, pageProps }) {
+  const trackUrl = async (path) => {
+    await fetch("/api/track", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ path: path }),
+    });
+  };
+
+  const sendMsg = (key, value) => {
+    localStorage.setItem(key, value);
+    localStorage.removeItem(key);
+  };
+
   useEffect(() => {
-    console.log("Start path", Router.pathname);
-    trackUrl(Router.pathname);
+    // trackUrl(Router.pathname);
+
+    Router.events.on("routeChangeComplete", trackUrl);
+
+    window.addEventListener("storage", function (e) {
+      if (e.key == "next_vercel_tracking: sId" && sID === undefined) {
+        console.log("got the id sent!");
+
+        sID = e.newValue;
+        trackUrl(Router.pathname);
+      }
+
+      if (e.key == "next_vercel_tracking: send_sId" && sID !== undefined) {
+        sendMsg("next_vercel_tracking: sId", sID);
+      }
+    });
+
+    console.log("Asked for the id");
+    sendMsg("next_vercel_tracking: send_sId", sID);
+
+    setTimeout(() => {
+      if (sID === undefined) {
+        sID = Math.floor(Math.random() * 100).toString();
+        trackUrl(Router.pathname);
+      }
+    }, 1000);
   }, []);
 
   return <Component {...pageProps} />;
